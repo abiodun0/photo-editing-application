a44ffc70bade0eff2b839a4d0920823ad2e73bd2
Abiodun Shuaib <abiodun.shauib.m@gmail.com>
2015-12-21 16:48:28 +0100

    [Feature #110405950] Database configuration setup

--- image_editor/userprofile/models.py
+++ image_editor/userprofile/models.py
@@ -1,5 +1,13 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User)
+    social_id = models.CharField(max_length=200, null=True)
+    image = models.CharField(max_length=200, null=True)
+
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
