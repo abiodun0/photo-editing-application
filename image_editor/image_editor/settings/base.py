9e5f7bde30fbc551ddf9dfeaee8a4a4bbafeb1b5
Abiodun Shuaib <abiodun.shauib.m@gmail.com>
2016-01-08 05:45:04 +0100

    [Feature #110018076] edit circuit yml and readme

--- image_editor/image_editor/settings/base.py
+++ image_editor/image_editor/settings/base.py
@@ -17,9 +17,6 @@ BASE_DIR = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
-PROJECT_ROOT = os.path.dirname(BASE_DIR)
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
